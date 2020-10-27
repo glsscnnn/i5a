845d136cf995ae33a23b9c9f400ca18f64391de3
loganj <loganj@localhost.localdomain>
2020-10-26 19:31:45 -0500

    第一

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const upload = (event: any) => {
+    // file
+    let file = event.target.files[0];
+
+    // create the form data to be sent
+    const formData = new FormData();
+    formData.append("myFile", file);
+
+    // send post request
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://localhost:8000/api", true);
+    xhr.send(formData);
+  };
+
+  const submit = async () => {
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="file" onChange={upload}/>
+      <br/>
+      <button onClick={submit}>Submit</button>
     </div>
   );
 }
